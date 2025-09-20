ff26cb410dd77fc32c5390aeebbaa2460b425a81
Quimbuadi <antonioquimbuadi@gmail.com>
2025-09-20 13:30:10 +0100

    adicionando os middlewares

--- app/app.js
+++ app/app.js
@@ -1,9 +1,15 @@
 import express from "express"
+import cors from "cors"
 import auth from "./routes/Auth.js"
+import helmet from "helmet"
 
 const app = express()
 
 //middlewares
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
+app.use(helmet())
+app.use(cors())
 app.use('/api', auth)
 
 
