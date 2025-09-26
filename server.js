f4dd86e27f9dae059fb989d230493b137dc47138
Quimbuadi <antonioquimbuadi@gmail.com>
2025-09-26 21:58:03 +0000

    mudando a porta

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import app from "./app/app.js";
 import connectDB from './app/infra/db.js';
 
 dotenv.config()
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 
 //conexao ao banco de dados
 connectDB()
