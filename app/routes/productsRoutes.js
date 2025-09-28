e329cedce80d755db65f3c999859f1277f28b34c
Quimbuadi <antonioquimbuadi@gmail.com>
2025-09-28 10:50:30 +0000

    construindo o upload de imagnes

--- app/routes/productsRoutes.js
+++ app/routes/productsRoutes.js
@@ -1,11 +1,12 @@
 import express from "express"
 import {createProduct, showAll, deleteProduct, searchProduct} from "../controllers/productController.js"
+import  upload  from "../middlewares/uploadMiddleware.js"
 
 const routes = express.Router()
 
-const product = routes.post('/products', createProduct)
+const product = routes.post('/products', upload.single("image"), createProduct)
 const show = routes.get('/products', showAll)
 const deleteOneProduct = routes.delete('/products/:id', deleteProduct)
 const findOneProduct = routes.get('/products/:name', searchProduct)
 
-export  {product, show, deleteOneProduct, searchProduct}
+export  {product, show, deleteOneProduct, findOneProduct}
