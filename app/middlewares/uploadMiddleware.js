e9368ba68bec200948ec71faa774514e32688dc6
Quimbuadi <antonioquimbuadi@gmail.com>
2025-10-15 20:21:04 +0000

    buscando produto pelo nome

--- app/middlewares/uploadMiddleware.js
+++ app/middlewares/uploadMiddleware.js
@@ -1,9 +1,10 @@
 import multer from "multer"
 import { extname, resolve } from "path"
-
+/*
 const storage = () => {
   return multer.diskStorage({
     destination: "./temp/uploads/products",
     filename: 
   })
 }
+  */
