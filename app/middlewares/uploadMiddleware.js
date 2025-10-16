9386bddb860ed6bd87c1199857dab26607124b8d
Quimbuadi <antonioquimbuadi@gmail.com>
2025-10-16 13:04:15 +0000

    cadastrar user e autenticar user ao mesmo tempo

--- app/middlewares/uploadMiddleware.js
+++ app/middlewares/uploadMiddleware.js
@@ -1,14 +1,26 @@
 import multer from "multer"
 import fs from "fs"
 import path from "path"
+import supabase from "../config/supabaseClient.js"
 
-async function uploadToSupabase(localPath, filename, mimeType) {
-  const { data , error } = await 
+async function uploadToSupabase(localPath, fileName, mimeType) {
+
+  const { data, error } = await supabase.storage
+    .from("product-image")
+    .upload(fileName, fs.createReadStream(localPath), {
+      contentType: mimeType,
+      cacheControl: "3600",
+      upsert: false
+    })
+  if (error) throw error
+  const { data: publicUrl } = supabase.storage.from('produtos').getPublicUrl(fileName)
+  return publicUrl.publicUrl
+
 }
 
 //salvar temporariamente no tmp
 
-const upload = multer({dest: '/tmp'})
+const upload = multer({ dest: '/tmp' })
 
 
 
