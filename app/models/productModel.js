fb4d05596d14dbb86dcf39e0d96e53d43119f033
Quimbuadi <antonioquimbuadi@gmail.com>
2025-10-22 21:10:30 +0000

    crud

--- app/models/productModel.js
+++ app/models/productModel.js
@@ -2,10 +2,37 @@ import db from "../infra/db.js"
 import mongoose from "mongoose"
 import categoryModel from "./categorieModel.js"
 
-npm install cloudinary
-# opcional para geração de URLs
-npm install @cloudinary/url-gen
-# opcional para integração com multer
-npm install multer multer-storage-cloudinary
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 2,
+        maxlength: 30,
+        unique: true
+    },
+    price: {
+        type: mongoose.Types.Decimal128,
+        required: true
+    },
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: categoryModel,
+        required: true
+    },
+
+    stock: {
+        type: Number,
+        required: true
+    },
+    image: {
+        type: [String],
+        required: false
+    },
+    description: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true }
+)
 
 export default mongoose.model("product", productSchema)
