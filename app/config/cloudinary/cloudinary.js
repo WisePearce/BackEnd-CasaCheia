dd00b0f0338290f2f480c2f60b280e6877bf96bf
Quimbuadi <antonioquimbuadi@gmail.com>
2025-11-01 18:11:13 +0000

    APY_KEY -> API_KEY

--- app/config/cloudinary/cloudinary.js
+++ app/config/cloudinary/cloudinary.js
@@ -5,7 +5,7 @@ import { v2 as cloudinary } from "cloudinary"
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_NAME,
-    api_key: process.env.APY_KEY,
+    api_key: process.env.API_KEY,
     api_secret: process.env.API_SECRET
 })
 console.log("teste da config: ", cloudinary)
