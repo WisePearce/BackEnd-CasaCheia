156c9be4905c9db9bf3423795fd06d3c3a4fdcc3
Quimbuadi <antonioquimbuadi@gmail.com>
2026-04-01 01:52:05 +0100

    variavel para o banco de dados

--- app/infra/db.js
+++ app/infra/db.js
@@ -1,18 +1,19 @@
 import mongoose from "mongoose";
 import dotenv from 'dotenv'
 dotenv.config()
+//nome do banco
+const database = process.env.DB_NAME;
 
 async function connectDB() {
 
     try {
-        const conn = await mongoose.connect(`${process.env.MONGODB_URL}ecomerce`, {
+        const conn = await mongoose.connect(`${process.env.MONGODB_URL}database`, {
             maxPoolSize: 10
         }
         )
         console.log('conected to database')
     } catch (error) {
         console.log(`error on connection`)
-        process.exit()
     }
 }
 
